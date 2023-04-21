62c3c2cc97a6d4f8c8176e16ced834f5941f9d84
Aleksandra Adaszek <aleksandra.adaszek@gmail.com>
2023-04-21 22:06:24 +0200

    19.2

--- src/main/java/com/crud/tasks/service/DbService.java
+++ src/main/java/com/crud/tasks/service/DbService.java
@@ -7,14 +7,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
-@RequiredArgsConstructor(onConstructor =@__(@Autowired))
+@RequiredArgsConstructor
 public class DbService {
 
     private final TaskRepository taskRepository;
 
     public List<Task> getAllTasks() {
         return taskRepository.findAll();
+    }
+    public Optional <Task> getTask (final long taskId) {
+        return taskRepository.findById(taskId);
     }
 }
